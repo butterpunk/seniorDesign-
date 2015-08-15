8b3a3afbb0c975e0079c67a8abeebec8305ecc9d
butterpunk <blakebutterworth@gmail.com>
2015-08-15 11:52:45 -0500

    stopped gps loop
    stopped gps loop. Now servo works, but gps doesn't

--- SeniorDesign_.ino
+++ SeniorDesign_.ino
@@ -1,24 +1,16 @@
+#include <Servo.h>
+
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-#include <Servo.h>
-/*
-   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
-static const int RXPin = 3, TXPin = 5;
+
+static const int RXPin = 5, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
 static const uint32_t destLat= 30.418080;
 static const uint32_t destLong= -91.167420; 
 
-// The TinyGPS++ object
 TinyGPSPlus gps;
-
-// Servo object
 Servo servo; 
-
-// The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
 void setup()
@@ -26,43 +18,50 @@ void setup()
   Serial.begin(115200);
   ss.begin(GPSBaud);
 
-  //servo.attach(9);
-
-  Serial.println(F("DeviceExample.ino"));
-  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
-  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
+  Serial.println(F("Senior Design "));
+  Serial.println(F("by Blake Butterworth test"));
   Serial.println();
+
+   displayInfo();
+
+   Serial.end();
+   ss.end();
+   
+  servo.attach(9);
 }
 
-void loop()
-{
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-      displayInfo();
-    
+void loop() {
+  
+  
 
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
-    Serial.println(F("No GPS detected: check wiring."));
-    while(true);
-  }
-}
+  servo.write(90);
+ }
+
+ 
+ // if (millis() > 5000 && gps.charsProcessed() < 10)
+ // {
+   // Serial.println(F("No GPS detected: check wiring."));
+   // while(true);
+ // }
+  
+
 
 void displayInfo()
 {
   Serial.print(F("Location: ")); 
+ 
   if (gps.location.isValid())
   {
     Serial.print(gps.location.lat(), 6);
     Serial.print(F(","));
     Serial.print(gps.location.lng(), 6);
-   servo.write(90);
+ 
+  
   }
   else
   {
     Serial.print(F("INVALID"));
+   
   }
 
   Serial.print(F("  Date/Time: "));
@@ -77,6 +76,7 @@ void displayInfo()
   else
   {
     Serial.print(F("INVALID"));
+    
   }
 
   Serial.print(F(" "));
@@ -100,4 +100,6 @@ void displayInfo()
   }
 
   Serial.println();
+
+
 }
